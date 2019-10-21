f8f907ad3832386dc79205426d8db2ca9fa85ba6
Arthur Iasbeck <arthuriasbeck@gmail.com>
2019-10-20 21:21:55 -0300

    Consegui implementar a função em que entro com theta e obtenho gama. Essa função vai ser usada tanto na computação da função objetivo quanto na computação de uma das restrições. Ela estava retornando um vetor vazio porque eu não estava inicializando as variáveis globais. Pra isso criei a função 'mainTest.m'. Agora vou implementar esta função na função objetivo e ver o que eu obtenho. Em seguida implemento na restrição. Pra testar a implementação desta função utilizarei a 'fObjTest.m'.

--- nLConstBP/link4.m
+++ nLConstBP/link4.m
@@ -1,22 +1,24 @@
 function gama = link4(theta,m,thetaR,l1)
 
-global h;
-global hm;
-global hc;
-global d3;
-global r0;
-global h0;
-global gama0;
+global h
+global hm
+global hc
+global d3
+global r0
+global h0
+global gama0
 
-l0 = sqrt((h - hm - hc)^2 + (m)^2);
+thetaN = atan((h - hm - hc)/m)
+
+l0 = sqrt((h - hm - hc)^2 + (m)^2)
 l2 = sqrt((l1*cos(thetaR) + d3 - m - r0)^2 + ...
-    (h - hm - l1*sin(thetaR) - h0)^2);
-l3 = sqrt(hc^2 + d3^2);
+    (h - hm - l1*sin(thetaR) - h0)^2)
+l3 = sqrt(hc^2 + d3^2)
 
-k1 = -2*l1*l3*sin(theta);
-k2 = 2*l3*(l0 - l1*cos(theta));
-k3 = l0^2 + l1^2 - l2^2 + l3^2 - 2*l0*l1*cos(theta);
+k1 = -2*l1*l3*sin(theta)
+k2 = 2*l3*(l0 - l1*cos(theta))
+k3 = l0^2 + l1^2 - l2^2 + l3^2 - 2*l0*l1*cos(theta)
 
-phi = 2*atan((-k1 - sqrt(k1.^2 + k2.^2 - k3.^2))./(k3 - k2));
+phi = 2*atan((-k1 - sqrt(k1.^2 + k2.^2 - k3.^2))./(k3 - k2))
 
-gama = pi - phi - gama0 - thetaN;
+gama = pi - phi - gama0 - thetaN
---
+++ nLConstBP/mainTest.m
@@ -0,0 +1,28 @@
+global h;
+global hm;
+global hc;
+global d3;
+global r0;
+global h0;
+global r;
+global cb;
+global ct;
+global gama0;
+
+h = 0.1625;
+hm = 0.0425;
+hc = 0.084;
+d3 = 0.15;
+r0 = 0.02;
+h0 = 0.03;
+r = 0.01;
+cb = 0.015;
+ct = 0.03;
+gama0 = atan2(hc,d3);
+
+thetaR = 0;
+m = 0.10;
+l1 = 0.05;
+theta = 30;
+
+link4(theta,m,thetaR,l1)
